de559eb560b92b4d1ee1ca5a436d444531adf1c3
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-06-06 18:50:41 +0300

    gitignore

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import firebaseConfigObj from './firebaseConfig'
+const firebaseConfig = firebaseConfige
 
-const firebaseConfig = {
-apiKey:"AIzaSyA0Hf1giRlNV63KdH6QSK3Mn82PUMLKRsM",
-authDomain:"e-commerce-87016.firebaseapp.com",
-projectId:"e-commerce-87016",
-storageBucket:"e-commerce-87016.appspot.com",
-messagingSenderId:"1053636809053",
-appId:"1:1053636809053:web:a5f81f0f601e4515a018b4",
-measurementId:"G-51CCL07QZ3"
-}
-
-const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfigObj);
 
 export const auth = app.auth();
 export default app;
